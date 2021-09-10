04e997ae7ef661b4d7ecc26e62e30b001397c616
vancovermobi <vancovermobi@gmail.com>
2021-09-10 21:10:52 +0700

    13-DeploytoHeroKu

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ server.use((req, res, next) => {
 
 // Use default router
 // server.use(router)
+const PORT = process.env.PORT || 4000
 server.use('/api',router)
-server.listen(4000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
