567f74fc362f54409edef351c7c66ff2ce9e5b5f
vancovermobi <vancovermobi@gmail.com>
2021-09-10 12:01:29 +0700

    07FakerRandomPproducts

--- generate-data.js
+++ generate-data.js
@@ -13,64 +13,66 @@ faker.local = "vi";
 // console.log(faker.image.imageUrl());
 // console.log(faker.name.findName());
 
-const randomCategoryList = (n) =>{
-    if(n<=0) return;
+const randomCategoryList = (n) => {
+  if (n <= 0) return;
 
-    const categoryList = [];
+  const categoryList = [];
 
-    // loop and push category
-    Array.from(new Array(n)).forEach(()=>{
-        const category = {
-            id: faker.random.uuid(),
-            name: faker.commerce.department(),
-            createAt: Date.now(),
-            updateAt: Date.now(),
-        };
+  // loop and push category
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.random.uuid(),
+      name: faker.commerce.department(),
+      createAt: Date.now(),
+      updateAt: Date.now(),
+    };
 
-        categoryList.push( category );
-    });
+    categoryList.push(category);
+  });
 
-    return categoryList;
+  return categoryList;
 };
-const randomProductList = (n) =>{
-    // if(n<=0) return;
+const randomProductList = (categoryList, numberProduct) => {
+  if (numberProduct <= 0) return;
 
-    // const ProductList = [];
+  const ProductList = [];
 
-    // // loop and push Product
-    // Array.from(new Array(n)).forEach(()=>{
-    //     const Product = {
-    //         categoryId: faker.random.uuid(),
-    //         id: faker.random.uuid(),
-    //         name: faker.commerce.productName(),
-    //         color: "cyan",
-    //         price: 930,
-    //         description: faker.commerce.productDescription(),
-    //         thumbnailUrl: faker.image.imageUrl(),
-    //         createAt: Date.now(),
-    //         updateAt: Date.now(),
-    //     };
-
-    //     ProductList.push( Product );
-    // });
-
-    return ProductList;
+  // // loop and push Product
+  for (const category of categoryList) {
+    Array.from(new Array(numberProduct)).forEach(() => {
+      const Product = {
+        categoryId: category.id,
+        id: faker.random.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        thumbnailUrl: faker.image.imageUrl(400, 400),
+        createAt: Date.now(),
+        updateAt: Date.now(),
+      };
+      ProductList.push(Product);
+    });
+  }
+  return ProductList;
 };
 
 // IFFE
 (() => {
   // random data
   const categoryList = randomCategoryList(4);
-  const productList = randomProductList(4);
+  const productList = randomProductList(categoryList, 5);
 
   // prepare db object
-//   const db = {
-//     categories: [],
-//     products: [],
-//     profile: {
-//       name: "Po",
-//     },
-//   };
+
+    // const db = {
+    //   categories: [],
+    //   products: [],
+    //   profile: {
+    //     name: "Po",
+    //   },
+    // };
+
   const db = {
     categories: categoryList,
     products: productList,
